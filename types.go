250f37361a6da5cc2d49c187a4c79363f59e89d0
YasserRABIE <yasserrabie110306@gmail.com>
2024-05-19 13:44:19 +0300

    Refactor Account struct and add CreateAccountRequest type

--- types.go
+++ types.go
@@ -1,20 +1,30 @@
 package main
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+)
 
-type Account struct {
-	ID        int    `json:"id"`
+type CreateAccountRequest struct {
 	FirstName string `json:"firstName"`
 	LastName  string `json:"lastName"`
-	Number    int64  `json:"number"`
-	Balance   int64  `json:"balance"`
+}
+
+type Account struct {
+	ID         int        `json:"id"`
+	FirstName  string     `json:"firstName"`
+	LastName   string     `json:"lastName"`
+	Number     int64      `json:"number"`
+	Balance    int64      `json:"balance"`
+	Updated_at *time.Time `json:"updated_at"`
+	Created_at time.Time  `json:"created_at"`
 }
 
 func NewAccount(firstName, lastName string) *Account {
 	return &Account{
-		ID:        rand.Intn(10000),
-		FirstName: firstName,
-		LastName:  lastName,
-		Number:    int64(rand.Intn(10000000)),
+		FirstName:  firstName,
+		LastName:   lastName,
+		Number:     int64(rand.Intn(10000000)),
+		Created_at: time.Now().UTC(),
 	}
 }
